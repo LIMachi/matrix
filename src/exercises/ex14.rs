10bccf4618debee5bade225d3d3adf7f784cc48f
LIMachi <lee.mach.hm@gmail.com>
2024-05-17 01:31:08 +0200

    exercises 14

--- src/exercises/ex14.rs
+++ src/exercises/ex14.rs
@@ -1,5 +1,22 @@
 use crate::matrix::Matrix;
 
 pub fn projection(fov: f32, ratio: f32, near: f32, far: f32) -> Matrix<4, 4, f32> {
-    todo!()
+    let s = 1. / (fov / 2.).tan();
+    let l = near - far;
+    Matrix::from([
+        [s, 0., 0., 0.],
+        [0., s / ratio, 0., 0.],
+        [0., 0., (far + near) / l, -1.],
+        [0., 0., 2. * near * far / l, 1.]
+    ])
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_projection() {
+        println!("{}", projection(80f32.to_radians(), 16./9., 0.1, 100.));
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,28 @@
+use std::fs::File;
+use std::io::Write;
+
 mod vector;
 mod matrix;
 mod exercises;
 mod utils;
 mod complex;
 
-fn main() {}
+fn ex14() {
+    let p = exercises::ex14::projection(80f32.to_radians(), 16./9., 0.1, 100.);
+    let mut s = String::new();
+    for r in 0..4 {
+        for c in 0..3 {
+            let v = p[(c, r)];
+            s += format!("{v}{}, ", if v.fract().abs() <= f32::EPSILON { "." } else { "" }).as_str();
+        }
+        let v = p[(3, r)];
+        s += format!("{v}{}\n", if v.fract().abs() <= f32::EPSILON { "." } else { "" }).as_str();
+    }
+    let mut file = File::create("./matrix_display/proj").unwrap();
+    file.write_all(s.as_bytes()).unwrap();
+    println!("generated file './matrix_display/proj', now use ./matrix_display/display (test executable provided by 42 for corrections)");
+}
+
+fn main() {
+    ex14();
+}
